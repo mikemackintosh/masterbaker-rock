fdd9a28f6ec90a9a62ae4588b895ce93a0b1ba37
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 13:30:39 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,12 @@ maintainer_email "m@splug.io"
 license          "Apache 2.0"
 description      "Manage Rock Runtimes for OS X"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 supports         "rock"
 
 depends "homebrew"
 
+# 0.2.2 ->
+#   - Fixed issue with node package name
 # 0.2.1 ->
 #   - Fixed issue with node package name
